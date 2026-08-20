79bbe47e84b182111dc1dc1e03d65117981dd139
AB-Ohi <askohi63907@gmail.com>
2026-08-20 13:19:33 +0600

    add the header section

--- src/Home/Header/Header.jsx
+++ src/Home/Header/Header.jsx
@@ -1,74 +1,49 @@
-"use client";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import React from "react";
+import { GoSearch } from "react-icons/go";
+import { BsForkKnife } from "react-icons/bs";
+import { LuCloudSunRain, LuLayers } from "react-icons/lu";
+import { HiOutlineColorSwatch } from "react-icons/hi";
+import headerImage from "../../../public/home/banner/bannerImg.png"
 import Image from "next/image";
 
-import banner1 from "../../../public/home/banner/banner1.jpg";
-import banner2 from "../../../public/home/banner/banner2.jpg";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import Link from "next/link";
-
 const Header = () => {
-  const HeaderImage = [
-    {
-      id: 1,
-      image: banner1,
-      bannerDetails: "Welcome to Wish Website details 1",
-      link:"#"
-    },
-    {
-      id: 2,
-      image: banner2,
-      bannerDetails: "Welcome to Wish Website details 2",
-      link:"#"
-    },
-    {
-      id: 3,
-      image: banner1,
-      bannerDetails: "Welcome to Wish Website details 3",
-      link:"#"
-    },
-  ];
-
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 4000 }}
-      loop
-      className="h-125 w-full"
-    >
-      {HeaderImage.map((Header) => (
-        <SwiperSlide key={Header.id}>
-          <div className="relative h-125 w-full">
-            <Image
-              src={Header.image}
-              alt={`Banner ${Header.id}`}
-              fill
-              priority
-              className="object-cover"
-            />
-
-            <div className="absolute inset-0 flex items-center justify-center flex-col bg-black/40">
-              <h1 className="text-4xl font-bold text-white">
-                {Header.bannerDetails}
-              </h1>
-              <Link
-              href={Header.link}
-                className="inline-flex items-center rounded-lg bg-sky-400 px-6 py-3 font-medium text-white transition-all duration-300 hover:bg-sky-500 hover:scale-105 hover:shadow-lg"
-              >
-                Visit Details
-              </Link>
-            </div>
+    <div className="flex items-center">
+      {/* header details*/}
+      <div className="">
+        <p>When your </p>
+        <p>Dream Wedding come true</p>
+        <p>
+          “ Once in a while, right in the middle of an ordinary life, love gives
+          us a fairy tale. ”
+        </p>
+        <div className="flex">
+          <input type="text" name="search" id="" />
+          <GoSearch />
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="flex items-center gap-2.5">
+            <HiOutlineColorSwatch />
+            Website Design and Invitations
           </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          <div className="flex items-center gap-2.5">
+            <BsForkKnife />
+            Cakes and Catering
+          </div>
+          <div className="flex items-center gap-2.5">
+            <LuCloudSunRain /> Rain Shaman
+          </div>
+          <div className="flex items-center gap-2.5">
+            <LuLayers />
+            Other services
+          </div>
+        </div>
+      </div>
+      {/* header Image */}
+      <div>
+        <Image src={headerImage}/>
+      </div>
+    </div>
   );
 };
 
